e677f95e49c9d1d44f0f22b36dbcfee9aec2b5a8
Miichone <mazma_5@hotmail.com>
2022-08-18 11:35:10 -0600

    RepasoLab2

---
+++ untitled4/src/main.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.util.Scanner;
+public class main {
+
+    public static void main(String[] args) {
+
+        /*int n1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer número: "));
+        int n2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo número: "));
+        int n3= n1+n2;*/
+
+        Scanner sc = new Scanner(System.in);
+
+        String list[] = new String[1000];
+        String numIngresado = sc.nextLine();
+
+
+        while(!numIngresado.equals(list)){
+
+            for(String numlist:list){
+
+            }
+
+        }
+
+
+
+
+
+
+
+        /*if(n3==100){
+            JOptionPane.showMessageDialog(null,"El resultado es 100");
+        }else if(n3>100){
+            JOptionPane.showMessageDialog(null,"El resultado es mayor de 100 porque el número es: "+n3);
+        }else{
+            JOptionPane.showMessageDialog(null,"El resultado es negativo porque el número es" +n3);
+        }*/
+
+
+    }
+}
